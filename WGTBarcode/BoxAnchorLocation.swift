26b8bb2be037e53522c18b248ea761592827aab0
Alonso <code@zhangwu.space>
2017-09-23 15:57:44 +0800

    First commit

---
+++ WGTBarcode/BoxAnchorLocation.swift
@@ -0,0 +1,17 @@
+//
+//  BoxAnchorLocation.swift
+//  WGTBarcode
+//
+//  Created by Alonso on 2017/9/23.
+//  Copyright © 2017年 Alonso. All rights reserved.
+//
+
+import Foundation
+
+enum BoxAnchorLocation {
+    case bottomLeft
+    case topLeft
+    case topRight
+    case bottomRight
+    case none
+}
